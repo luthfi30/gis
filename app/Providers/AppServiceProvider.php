73861b9c4137fd8c8d7b30d1732fc73ad4d62cc0
luthfi30 <lurrah30@gmail.com>
2025-12-16 12:28:03 +0700

    Fix layout broken by forcing HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -27,7 +28,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+        
+        // Policy Settings (Punya Anda yang lama)
         Gate::policy(Role::class, RolePolicy::class);
         Gate::policy(Permission::class, PermissionPolicy::class);
+
+        // === TAMBAHAN WAJIB UNTUK RAILWAY ===
+        // Kode ini memaksa Laravel menggunakan HTTPS saat di Production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
